00721d5741e9c3e0cf761045cbab6cdbf29027f9
l-shyu <lshyu@ncsu.edu>
2022-02-13 13:32:17 -0500

    Add recursive loop for 10000 times

--- MQTT2_client.py
+++ MQTT2_client.py
@@ -1,26 +1,83 @@
+
 import paho.mqtt.client as mqtt
 import time
 from datetime import datetime
 import sys
 import subprocess
+import os
+from pathlib import Path
+import traceback
+
+
+#output = sys.argv[3]
+count = 0
 
 def on_message(client, userdata, message):
     #print("received message: " ,str(message.payload.decode("utf-8")))
+    global count
+    try:
+        with open("time.txt", "a") as f:
+            #f.write("Receive: " + time.ctime() + "\n")
+            f.write("Receive: " + str(datetime.now()) + "\n")
+            count += 1
+    except:
+        pass
     file = message.payload
-    sys.stdout.buffer.write(file)
+    #sys.stdout.buffer.write(file)
+    content = file
+    #sys.stdout.buffer.write(file)
+    if count <= 1:
+        sys.stdout.buffer.write(file)
+    """
+    if count >= 1:
+        #sys.stdout.buffer.write(file)
+        #sys.stdout.flush()
+        pass
+    else:
+        #sys.stdout.flush()
+        try:
+            sys.stdout.buffer.write(file)
+        except:
+            #traceback.print_exc()
+            pass
+        #sys.stdout = os.fdopen(sys.stdout.fileno(), 'wb', buffering=1)
+    #count += 1
+    """
+   
+    """
+    if os.path.getsize("output.txt") <= 5:
+        sys.stdout.buffer.write(file)
+        print(os.path.getsize("output.txt"))
+        #print(os.stat("output_100B").st_size)
+        pass
+    else:
+        #print("larger")
+        pass
+    """
+    """
     with open("time.txt", "a") as f:
-        f.write("Receive: " + time.ctime() + "\n")
+        #f.write("Receive: " + time.ctime() + "\n")
+        f.write("Receive: " + str(datetime.now()) + "\n")
+    """
 #print(time.time())
 #print(datetime.now())
 #mqttBroker ="mqtt.eclipseprojects.io"
-#mqttBroker = "raspberrypi"
+mqttBroker = "raspberrypi"
 #mqttBroker = "34.68.50.129"
-mqttBroker = "127.0.0.1"
+#mqttBroker = "127.0.0.1"
+#mqttBroker = "10.2.1.255"
 client = mqtt.Client("Smartphone")
-client.connect(mqttBroker) 
-client.loop_start()
-client.subscribe("TEMPERATURE", qos=2)
-client.on_message=on_message
+client.connect(mqttBroker)
+#print(type(os.stat("output.txt").st_size))
+#print(os.stat("output.txt").st_size)
+#print(os.path.getsize("output.txt"))
+outputFile = "output.txt"
 
-time.sleep(30)
-client.loop_stop()
+#while True:
+while count < 10000:
+    client.loop_start()
+    client.subscribe("TEMPERATURE", qos=2)
+    client.on_message=on_message
+
+    #time.sleep(2)
+    client.loop_stop()
--- MQTT2_server.py
+++ MQTT2_server.py
@@ -2,35 +2,64 @@ import paho.mqtt.client as mqtt
 from random import randrange, uniform
 import time
 import sys
+from datetime import datetime
+import traceback
+#from scapy.all import *
+
+count = 0
 
 def on_message(client, userdata, message):
-    print("!!!!!!!!!!!!!!!!!!!!!!!")
+    try:
+        print("!!!!!!!!!!!!!!!!!!!!!!!")
+    except:
+        traceback.print_exc()
     #print("received message: " ,str(message.payload.decode("utf-8")))
 
 #mqttBroker ="mqtt.eclipseprojects.io" 
 #mqttBroker = "10.2.1.255"
-#mqttBroker = "raspberrypi"
+mqttBroker = "raspberrypi"
 #mqttBroker = "34.68.50.129"
 #mqttBroker = "34.68.50.129"
 #mqttBroker = "10.0.2.15"
-mqttBroker = "127.0.0.1"
+#mqttBroker = "127.0.0.1"
 #port = 1883
+
+#packets = sniff(filter = "host 127.0.0.1")
 
 client = mqtt.Client("Temperature_Inside")
 client.connect(mqttBroker) 
 #client.connect(mqttBroker, port)
+#file = sys.stdin.buffer.read(1024*1024*256)
+file = sys.stdin.buffer.read(256)
 
-while True:
+#while True:
+while count < 10500:
     randNumber = uniform(20.0, 21.0)
-    file = sys.stdin.buffer.read(1024*1024*1024)
+    #file = sys.stdin.buffer.read(1024*1024)
+
+    with open("time.txt", "a") as f:
+        f.write("Send: " + str(datetime.now()) + "\n")
+    client.subscribe("#", qos=2)
+    client.publish("TEMPERATURE", file, qos=2)
+    client.loop_start()
+    client.on_message=on_message
+    client.loop_stop()
+    print("SENT")
+    """
     with open("time.txt", "a") as f:
         client.subscribe("#", qos=2)
         client.publish("TEMPERATURE", file, qos=2)
         print("SENT")
         #client.loop()
+        
         client.loop_start()
         client.on_message = on_message
         client.loop_stop()
-        f.write("Send: " + time.ctime() + "\n")
-
-    time.sleep(5)
+        
+        #f.write("Send: " + time.ctime() + "\n")
+        f.write("Send: " + str(datetime.now()) + "\n")
+    """
+    print(count)
+    count += 1
+    time.sleep(0.8)
+    #print(packets)
