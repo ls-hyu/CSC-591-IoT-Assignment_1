6aee1d4ae97ce741fc06545e7544eceac64a9cb5
adamyen <adamyen214@hotmail.com>
2022-02-18 10:26:06 -0500

    [feat]: record time when send and receive file

--- mqtt_qos1/sub.py
+++ mqtt_qos1/sub.py
@@ -1,38 +1,46 @@
-# python3.6
-
 import random
-
+import asyncio
+import sys
+import time
 from paho.mqtt import client as mqtt_client
+
+experiment_file = str(sys.argv[1])
+experiment_reps = int(sys.argv[2])
 
 broker = 'localhost'
 port = 1883
 topic = "file_transfer"
-# generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
-f_out = open("./sub/1MB","wb") #use a different filename
+time_records = []
+count = 0
 
-def connect_mqtt() -> mqtt_client:
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client(client_id)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
-
-def on_message(client, userdata, msg):
-    f_out.write(msg.payload)
-    
+def on_message(client, userdata, message):
+    global count
+    time_records.append(time.time())
+    count += 1
+    print(count)
+    if count == experiment_reps:
+        record_time()
+        
+def record_time():
+    f = open("end_time.txt", "a")
+    for t in time_records:
+        f.write(str(t)+"\n")
+    f.close()
 
 def run():
-    client = connect_mqtt()
+    client = mqtt_client.Client(client_id)
+    client.connect(broker, port)
+    client.on_connect = on_connect
     client.subscribe(topic)
     client.on_message = on_message
     client.loop_forever()
 
 if __name__ == '__main__':
     run()
-    f_out.close()
